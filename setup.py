a369b757521359aad1f34be44fbb3662488abe80
jweatherby <jweatherby@gadventures.com>
2018-08-30 17:22:32 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='django-dispatcher',
-    version='0.1.0',
+    version='0.1.1',
     description='Python client for the G Adventures REST API',
     long_description=readme + '\n\n' + history,
     author='G Adventures',
