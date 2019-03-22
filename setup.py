22fc6201a9c8b45e36d9c0f90c7d07503c6d430b
jweatherby <jweatherby@gadventures.com>
2019-03-22 14:48:42 -0400

    Version bump to 0.4.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='django-dispatcher',
-    version='0.4.1',
+    version='0.4.2',
     description='Django logic engine (used for email logic)',
     long_description=readme + '\n\n' + history,
     author='G Adventures',
